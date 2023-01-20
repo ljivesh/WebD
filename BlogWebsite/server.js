b3daec3bdb46aacafd5d60ae206b17e184e7e99b
Menma <ljivesh@gmail.com>
2023-01-20 17:36:23 +0530

    Completed my own api

--- BlogWebsite/server.js
+++ BlogWebsite/server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const _ = require('lodash');
+const mongoose = require('mongoose');
 
 const homeContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-const posts = [];
+const postSchema = {
+    title: String,
+    content: String
+};
+mongoose.connect("mongodb://127.0.0.1:27017/blogWebsite");
+const Post = mongoose.model('post', postSchema);
+
 
 
 const app = express();
@@ -13,37 +20,83 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname+'/public/'));
 app.set('view engine', 'ejs');
 
-const ejsContent = {
-    homeContent: homeContent,
-    contactContent: homeContent,
-    aboutContent: homeContent,
-    posts: posts
-};
 
-app.get('/:locName', (req, res) => {
-    res.render(req.params.locName, ejsContent);
-});
-app.get('/posts/:postTitle', (req, res) => {
-    posts.forEach((post) => {
-        
-        if(_.lowerCase(post.title)===_.lowerCase(req.params.postTitle)) {
-            const data = {
-                postTitle: post.title,
-                postContent: post.content
+app.get('/', (req, res)=> {
+
+    Post.find({}, (err, posts)=> {
+        if(err) console.log(err);
+        else {
+            const homeData = {
+                homeContent: homeContent,
+                posts: posts
             };
-            res.render('post', data);
+            res.render('home', homeData);
         }
     });
 });
 
+app.get('/:locName', (req, res) => {
+    
+    const locName = req.params.locName;
+    
+    if(locName === 'about') {
+        const aboutData = {
+            aboutContent: homeContent
+        };
+        res.render('about', aboutData);
+    }
+
+    else if(locName === 'contact') {
+        const contactData = {
+            contactContent: homeContent
+        };
+        res.render('contact', contactData);
+    }
+
+    else if(locName === 'compose') {
+        res.render('compose');
+    }
+
+    else {
+        res.redirect('/');
+    }
+});
+
+// app.get('/posts/:postTitle', (req, res) => {
+
+//     const postTitle = req.params.postTitle;
+//     Post.findOne({title: postTitle}, (err, foundPost)=> {
+//         if(err) console.log(err)
+//         else if(foundPost!==null) {
+//             res.render('post', foundPost);
+//         }
+//         else res.render('postNotFound', {title: postTitle});
+//     });
+// });
+
 app.post('/compose', (req, res)=> {
+
     const composeData = {
         title: req.body.title,
         content: req.body.content
     };
-    posts.push(composeData);
+
+    const post = new Post(composeData);
+    post.save();
+    // posts.push(composeData);
     // console.log(`${composeData.title}:\n${composeData.content}`);
-    res.redirect('/compose');
+    res.redirect('/');
+});
+
+app.post('/post', (req, res)=> {
+    const postId = req.body.postId;
+    Post.findById(postId, (err, foundPost)=> {
+        if(err) console.log(err);
+        else {
+            // console.log("Reached HEREEEEEE");
+            res.render('post', foundPost);
+        }
+    });
 });
 
 const port = 3000;
